7789d43349716b9705bd8bd8e0e2734688f05253
BAMBA928 <bambapartou42@gmail.com>
2026-08-16 19:38:49 +0000

    feat(pos): finalisation du controleur et de l'interface de caisse tactile

--- public/index.php
+++ public/index.php
@@ -1,41 +1,68 @@
 <?php
 
-require_once dirname(__DIR__). '/src/Model/Entity/Role.php';
-require_once  dirname(__DIR__). '/src/core/DataBase.php';
-require_once  dirname(__DIR__).  '/src/Model/Entity/Produit.php';
-require_once dirname(__DIR__).  '/src/Model/Repository/ProduitRepository.php';
-require_once dirname(__DIR__).  '/src/Service/VentesService.php';
 
-use src\Model\Entity\Role;
+session_start();
+ require dirname(__DIR__).'/src/core/DataBase.php';
+require_once dirname(__DIR__) . '/src/Model/Entity/Produit.php';
+require_once dirname(__DIR__) . '/src/Model/Entity/Client.php';
+require_once dirname(__DIR__) . '/src/Model/Entity/ModePaiement.php';
+require_once dirname(__DIR__) . '/src/Model/Repository/ProduitRepository.php';
+require_once dirname(__DIR__) . '/src/Model/Repository/ClientRepository.php';
+require_once dirname(__DIR__) . '/src/Model/Repository/ModePaiementRepository.php';
+require_once dirname(__DIR__) . '/src/Service/VentesService.php';
+require_once dirname(__DIR__) . '/src/contrloller/POSController.php';
 
-// $role = new Role();
+use src\Controller\POSController;
 
-// var_dump($role->afficherId());
-use src\Service\VenteService;
-use src\Model\Entity\Produit;
-use src\Model\Repository\ProduitRepository;
-use src\Core\Database;
-$pdo = Database::getInstance();
-// echo "Driver : " . $pdo->getAttribute(PDO::ATTR_DRIVER_NAME) . PHP_EOL;
-// $repo = new ProduitRepository();
-// $produits=$repo->trouverTous();
-// var_dump($produits);
-// trouverTous();
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$method = $_SERVER['REQUEST_METHOD'];
 
-$service = new VenteService($pdo);
-$panier = [
-    [
-        'produit_id' => 99999,
-        'quantite' => 2
-    ]
-];
+$controller = new POSController();
 
-$service->enregistrerVente($panier);
+if ($method === 'GET' && $uri === '/') {
 
-// $service->enregistrerVente([]);
-var_dump($service);
+    $controller->index();
 
-echo "Transaction terminée" . PHP_EOL;
+} elseif ($method === 'POST' && $uri === '/pos/vente') {
+
+    $controller->enregistrer();
+
+} else {
+
+    http_response_code(404);
+
+    echo 'Page non trouvée.';
+}
+// use src\Model\Entity\Role;
+
+// // $role = new Role();
+
+// // var_dump($role->afficherId());
+// use src\Service\VenteService;
+// use src\Model\Entity\Produit;
+// use src\Model\Repository\ProduitRepository;
+// use src\Core\Database;
+// $pdo = Database::getInstance();
+// // echo "Driver : " . $pdo->getAttribute(PDO::ATTR_DRIVER_NAME) . PHP_EOL;
+// // $repo = new ProduitRepository();
+// // $produits=$repo->trouverTous();
+// // var_dump($produits);
+// // trouverTous();
+
+// $service = new VenteService($pdo);
+// $panier = [
+//     [
+//         'produit_id' => 99999,
+//         'quantite' => 2
+//     ]
+// ];
+
+// $service->enregistrerVente($panier);
+
+// // $service->enregistrerVente([]);
+// var_dump($service);
+
+// echo "Transaction terminée" . PHP_EOL;
 
 // trouverParId(99999);
 // // creer();
